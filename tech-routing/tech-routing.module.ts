af8db6891263b000cc9882976a405a104c1b3401
bhargawarao1999 <85075303+bhargawarao1999@users.noreply.github.com>
2021-07-06 17:51:53 +0530

    Initial commit

---
+++ tech-routing/tech-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { WebComponent } from '../web/web.component';
+import { JavaComponent } from '../java/java.component';
+import { ProductsComponent } from '../products/products.component';
+import { AdvJavaComponent } from '../adv-java/adv-java.component';
+import { CoreJavaComponent } from '../core-java/core-java.component';
+import { HtmlCourseComponent } from '../html-course/html-course.component';
+import { CssCourseComponent } from '../css-course/css-course.component';
+
+const routes: Routes = [
+  { path: 'web', component: WebComponent},
+  { path: 'java', component: JavaComponent},
+  { path: 'product', component: ProductsComponent }
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class TechRoutingModule { }
+export const routingComponents = [WebComponent, JavaComponent]
